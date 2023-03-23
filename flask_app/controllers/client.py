78d414ee74075697093ac70fa47e1b44ea000a66
tmchuynh <tinamchuynh@gmail.com>
2023-03-23 14:17:57 -0700

    📦 NEW: register clients

--- flask_app/controllers/client.py
+++ flask_app/controllers/client.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from flask_app import app
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 from flask_app.models.client_model import Client
 
+@app.route('/employee/add_client', methods=['POST'])
+def add_client():
+    if not Client.validate_client(request.form):
+        return redirect('/employee/home')
+    new_client = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email']
+    }
+    Client.create_client(new_client)
+    flash('Client added successfully', 'client_added')
+    return redirect('/employee/home')
--- flask_app/models/client_model.py
+++ flask_app/models/client_model.py
@@ -2,6 +2,10 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import DATABASE
 from flask import flash
 
+from flask_app.models import employee_model
+
+import re
+
 class Client:
     def __init__(self, data):
         self.first_name = data['first_name']
@@ -9,6 +13,8 @@ class Client:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        self.employee = None
+        
         
     @classmethod
     def get_all_clients(cls):
@@ -19,6 +25,7 @@ class Client:
             list_of_clients.append(cls(result))
         return list_of_clients
     
+    
     @classmethod
     def get_client_by_id(cls, data):
         query = "SELECT * FROM clients WHERE id = %s"
@@ -27,17 +34,50 @@ class Client:
             return None
         return cls(results[0])
     
+    
+    @classmethod
+    def get_client_by_employee(cls, data):
+        query = """SELECT * FROM clients
+        LEFT JOIN client_employee_relationship ON client_employee_relationship.clients_id = clients.id
+        LEFT JOIN employee ON client_employee_relationship.employee_id = employee.id
+        WHERE employee.id = %(employee_id)s"""
+        
+        results = connectToMySQL(DATABASE).query_db(query, data)
+        
+        if len(results):
+            client = cls(results[0])
+            client.employee = []
+            
+            for result in results:
+                if not result['employee_id']:
+                    break
+                
+                employee = {
+                    'id': result['id'],
+                    'first_name': result['first_name'],
+                    'last_name': result['last_name'],
+                    'email': result['email'],
+                    'created_at': result['created_at'],
+                    'updated_at': result['updated_at']
+                }
+                client.employee.append(employee_model.Employee(employee))
+            return client
+        return None
+
+    
     @classmethod
     def create_client(cls, data):
         query = "INSERT INTO clients (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s)"
         results = connectToMySQL(DATABASE).query_db(query, data)
-        return cls(results)
+        return results
+    
     
     @classmethod
     def update_client(cls, data):
         query = "UPDATE clients SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(client_id)s"
         results = connectToMySQL(DATABASE).query_db(query, data)
         return cls(results)
+    
     
     @classmethod
     def delete_client(cls, data):
@@ -46,3 +86,49 @@ class Client:
         return cls(results)
 
     
+    @classmethod
+    def check_database(cls, data):
+        query = "SELECT * FROM clients WHERE  email = %(email)s"
+        results = connectToMySQL(DATABASE).query_db(query, data)
+        if results:
+            return True
+        return False
+        
+        
+    @staticmethod
+    def validate_client(data):
+        regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+        
+        is_valid = True
+        
+        if (regex.search(data['first_name']) != None or len(data['first_name']) < 2):
+            # first name contains special characters
+            flash('First name contains special characters or does not meet minimum length requirements.', "new_client")
+            is_valid = False
+        
+        if (regex.search(data['last_name'])!= None or len(data['last_name']) < 2):
+            # last name contains special characters
+            flash('Last name contains special characters or does not meet minimum length requirements.', "new_client")
+            is_valid = False
+        
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        
+        if (re.fullmatch(regex, data['email'])):
+            if not data['email'] == data['email_confirmation']:
+                flash('Emails do not match', "new_client")
+                is_valid = False
+            # email is valid
+            # need to check if the email is already in use
+            this_user = {
+                'email': data['email']
+            }
+            results = Client.check_database(this_user)
+            
+            if results:
+                flash('Email is already in use, please use a different email', "new_client")
+                is_valid = False
+        else:
+            flash('Email contains special characters', "new_client")
+            is_valid = False
+        
+        return is_valid
